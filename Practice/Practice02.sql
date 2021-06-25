95457141caf55f9f25b1c47a4bccfb61fa496320
rlatjgus960 <rlatjgus960@naver.com>
2021-06-25 16:49:15 +0900

    6/25일 Practice02

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -1,17 +1,18 @@
+--ex01
 select count(manager_id)
 from employees;
 
-
+--ex02
 select  max(salary) 최고임금,
         min(salary) 최저임금,
         max(salary)-min(salary) "최고임금-최저임금"
 from employees;
 
-
+--ex03
 select to_char(max(hire_date),'YYYY"년" MM"월" DD"일"')
 from employees;
 
-
+--ex04
 select  avg(salary), 
         max(salary), 
         min(salary), 
@@ -20,7 +21,7 @@ from employees
 group by department_id
 order by department_id desc;
 
-
+--ex05
 select  avg(salary), 
         max(salary), 
         min(salary), 
@@ -29,11 +30,11 @@ from employees
 group by job_id
 order by min(salary) desc, round(avg(salary), 0) asc;
 
-
+--ex06
 select to_char(min(hire_date),'YYYY-MM-DD DAY')
 from employees;
 
-
+--ex07
 select  department_id,
         avg(salary), 
         min(salary), 
@@ -43,7 +44,7 @@ group by department_id
 having avg(salary)-min(salary) < 2000
 order by avg(salary)-min(salary) desc;
 
-
+--ex08
 select  max(max_salary),
         min(min_salary),
         max(max_salary)-min(min_salary)
@@ -53,7 +54,7 @@ order by max(max_salary)-min(min_salary) desc;
 
 
 
-
+--ex09
 select round(avg(salary),0), min(salary), max(salary)
 from employees
 where hire_date > '2004-12-31'
@@ -62,7 +63,7 @@ having avg(salary)> 5000
 order by avg(salary) desc;
 
 
-/*문제10
+/*ex10
 아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다. 
 입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’ 
 이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
@@ -71,9 +72,10 @@ order by avg(salary) desc;
 select  first_name,
         hire_date,
         case when hire_date < '03-01-01' then '창립멤버'
-             when hire_date between '03-01-01' and '03-12-31' then '03년입사'
-             when hire_date between '04-01-01' and '04-12-31' then '04년입사'
+             when hire_date < '04-01-01' then '03년입사'
+             when hire_date < '05-01-01' then '04년입사'
              else '상장이후입사'
         end optDate
 from employees
 order by hire_date asc;
+
